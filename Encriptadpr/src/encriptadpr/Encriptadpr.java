b1b8c26fd89ae2f4b5f627159054fd229038360f
unknown <trialsgama@gmail.com>
2018-12-06 13:13:49 -0300

    Carpeta

---
+++ Encriptadpr/src/encriptadpr/Encriptadpr.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encriptadpr;
+
+import org.hyperic.sigar.*;
+import java.security.*;
+import java.io.*;
+
+
+
+
+/**
+ *
+ * @author Lean
+ */
+public class Encriptadpr {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    }
+    
+}
